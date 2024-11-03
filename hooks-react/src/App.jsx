e29bd0973cf5c3b4a01fbf2addc0212152bb9564
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-11-03 15:13:44 +0530

    Hooks useState

---
+++ hooks-react/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+function App() {
+  let [counter, setCounter] = useState(15)
+  // let counter = 5;
+
+  const addValue=()=>{
+    
+    if(counter>=20){
+      alert("Counter can't store negative value ")
+    }else{
+      counter++
+    }
+    console.log(counter);
+    
+    return setCounter(counter);
+
+  }
+  const removeValue=()=>{
+    
+    if(counter<=0){
+      alert("Counter can't store negative value ")
+    }else{
+      counter--
+    }
+    console.log(counter);
+    
+    return setCounter(counter);
+  }
+
+  return (
+    <>
+      <h1>Chai aur React </h1>
+      <h3>Counter Value: {counter}</h3>
+      <button onClick={addValue} >Add Value{counter}</button>
+      <br/>
+      <button onClick={removeValue} >Remove Value {counter}</button>
+      <footer>
+        footer: {counter}
+      </footer>
+    </>
+  )
+  
+}
+
+export default App
